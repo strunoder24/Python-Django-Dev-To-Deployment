c0d050fe7e043c8f6498afa0cfc51bc721cc49cb
Виктор Кондик <strunoder24@mail.ru>
2019-04-02 12:59:57 +1000

    listing and realtors models created and migrated

--- btre/settings.py
+++ btre/settings.py
@@ -78,12 +78,28 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'btre_local',
+            'USER': 'postgres',
+            'PASSWORD': '',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        },
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'btre_prod',
+            'USER': 'superadmin',
+            'PASSWORD': 'air3N97r',
+            'HOST': 'postgres',
+            'PORT': '5432',
+        },
+    }
 
 
 # Password validation
